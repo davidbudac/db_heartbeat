db7824b9ada11f36d748052f8c13b33dd1622955
David Budáč <davidbudac@gmail.com>
2025-02-18 21:27:35 +0100

    fix

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 export ORACLE_HOME=/mnt/c/app/client/david/product/19.0.0/client_1
 
-# cp $ORACLE_HOME/jdbc/lib/ojdbc8.jar ./lib
-# cp $ORACLE_HOME/jlib/oraclepki.jar ./lib
-# cp $ORACLE_HOME/jlib/osdt_core.jar ./lib
-# cp $ORACLE_HOME/jlib/osdt_cert.jar ./lib
+cp $ORACLE_HOME/jdbc/lib/ojdbc8.jar ./lib
+cp $ORACLE_HOME/jlib/oraclepki.jar ./lib
+cp $ORACLE_HOME/jlib/osdt_core.jar ./lib
+cp $ORACLE_HOME/jlib/osdt_cert.jar ./lib
 
 # Compile
 $ORACLE_HOME/jdk/bin/javac.exe -cp ".;./lib/oraclepki.jar;./lib/osdt_core.jar;./lib/osdt_cert.jar;./lib/ojdbc8.jar" ./OracleDBConnect.java 
